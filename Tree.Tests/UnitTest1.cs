fd431efd3fd0a6f270612c6560cead31d0e9a5c2
Home <allosipyan@mail.ru>
2020-04-14 19:45:33 +0500

    Test2

--- Tree.Tests/UnitTest1.cs
+++ Tree.Tests/UnitTest1.cs
@@ -16,5 +16,25 @@ namespace Tree.Tests
             Assert.AreEqual(expectedColor, resultColor);
 
         }
+
+        [TestMethod]
+        public void AddWithRedUncleAndRedParent()
+        {
+            //должен помен€тьс€ цвет у родител€  и д€ди на черный 
+            //дл€ сохранени€ свойства "ќба потомка каждого красного узла Ч чЄрные."
+            TreeAlgorithm tree = new TreeAlgorithm();
+
+            tree.Add(56);//root black
+            tree.Add(12);//red
+            tree.Add(65);//red
+            tree.Add(16);
+            var result = tree.Root;
+            Assert.AreEqual(56, result.Value);
+            Assert.AreEqual(12, result.Left.Value);
+            Assert.AreEqual(Color.B, result.Left.Colour);//цвет родител€
+            Assert.AreEqual(65, result.Right.Value);
+            Assert.AreEqual(Color.B, result.Left.Colour);//цвет д€ди
+            Assert.AreEqual(16, result.Left.Right.Value);
+        }
     }
 }
